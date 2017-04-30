95dda257866a37c0f3f5c48f7caf41fd7d555bb1
Hugo Peters <hpeters1024@gmail.com>
2017-04-30 17:33:14 +0200

    First Commit

---
+++ game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Template {
+
+class Game
+{
+	// member variables
+	public Surface screen;
+	// initialize
+	public void Init()
+	{
+	}
+	// tick: renders one frame
+	public void Tick()
+	{
+		screen.Clear( 0 );
+		screen.Print( "hello world", 2, 2, 0xffffff );
+        screen.Line(2, 20, 160, 20, 0xff0000);
+	}
+}
+
+} // namespace Template
