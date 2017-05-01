e37f66f85f86bbda45bf2bd73f971c4353f34344
Hugo Peters <hpeters1024@gmail.com>
2017-05-01 15:19:54 +0200

    Up to exercise 5.

---
+++ PlottedLine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    /// <summary>
+    /// A function wrapper for a plotted line.
+    /// </summary>
+    class PlottedLine
+    {
+        public float translation;
+        public float Function(float x)
+        {
+            return (float)Math.Sin(x - translation) + (float)Math.Sin((x - translation) * 5);
+        }
+
+        public PlottedLine()
+        {
+            translation = 0;
+        }
+    }
+}
--- game.cs
+++ game.cs
@@ -1,53 +1,104 @@
 ﻿using System;
 using System.IO;
 using System.Drawing;
+using System.Collections;
 using OpenTK;
+using OpenTK.Input;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
+
 namespace Template {
 
 class Game
 {
+        const float TRANSLATION_SPEED = 0.05f;
+        const float ZOOM_SPEED = 0.05f;
+
         float x1 = -1;
         float x2 = 1;
         float y1 = 1;
         float y2 = -1;
         float a = 0;
         OpenTKApp app;
+        PlottedLine func;
+        float rangeX = 4, rangeY = 4;
+        int centerX = 0, centerY = 0;
+        KeyboardState KeyPress, PrevKeyPress;
         // member variables
         public Surface screen;
         public Game(OpenTKApp app)
         {
-            this.app = app;
+            this.app = app; //Inherited the app pointer to get the window size
         }
 	    // initialize
 	    public void Init()
 	    {
-
+            func = new PlottedLine();
         }
 
 	    // tick: renders one frame
         public void Tick()
 	    {
-            a += 0.02f;
-            screen.Clear( 0 );
-            screen.Print(((float)app.Height).ToString(), 2, 2, 0xffffff );
+            //An aesthetically pleasing checker pattern background
+            screen.Clear(0);
+            for (int y = 0; y < screen.height; ++y)
+                for (int x = 0; x < screen.width; ++x)
+                    screen.Plot(x, y, (x ^ y)%256);
+
+            HandleKeyBoard(); //update keyboard status
+            screen.Print("Function Plotting", 2, 2, 0xffffff );
             screen.Line(2, 20, 160, 20, 0xff0000);
-            float centerX = 0, centerY = 0;
-            int rx1 = TX((float)(x1 * Math.Cos(a) + y1 * Math.Sin(a)), centerX);
-            int ry1 = TY((float)(x1 * Math.Sin(a) - y1 * Math.Cos(a)), centerY);
-            int rx2 = TX((float)(x1 * Math.Cos(a) + y2 * Math.Sin(a)), centerX);
-            int ry2 = TY((float)(x1 * Math.Sin(a) - y2 * Math.Cos(a)), centerY);
-            int rx3 = TX((float)(x2 * Math.Cos(a) + y2 * Math.Sin(a)), centerX);
-            int ry3 = TY((float)(x2 * Math.Sin(a) - y2 * Math.Cos(a)), centerY);
-            int rx4 = TX((float)(x2 * Math.Cos(a) + y1 * Math.Sin(a)), centerX);
-            int ry4 = TY((float)(x2 * Math.Sin(a) - y1 * Math.Cos(a)), centerY);
+            screen.Print("f(x) = Sin(x) + Sin(5x)", 2, 24, 0xffffff);
+
+            //This will plot the func PlottedLine by connecting straight lines
+            //between arbitrarily small values to create the illusion of a small line;
+            float stepSize = rangeX / screen.width;
+            for(float x=-rangeX; x < rangeX; x += stepSize)
+            {
+                float y1 = func.Function(x); //first y value
+                float y2 = func.Function(x + stepSize); //second y value
+                int drawX1 = TX(x, rangeX, centerX); //convert x to pixel value
+                int drawY1 = TY(y1, rangeY, centerY); //conver y to pixel value;
+                int drawX2 = TX(x + stepSize, rangeX, centerX); //second x value
+                int drawY2 = TY(y2, rangeY, centerY);  //second y value
+                screen.Line(drawX1, drawY1, drawX2, drawY2, 0xffff00); //plot a line between to the points
+            }
+
+            if (KeyPress.IsKeyDown(Key.Left))
+                func.translation += TRANSLATION_SPEED;
+
+            if (KeyPress.IsKeyDown(Key.Right))
+                func.translation -= TRANSLATION_SPEED;
+
+            if (KeyPress.IsKeyDown(Key.A))
+            {
+                rangeX += ZOOM_SPEED;
+                rangeY += ZOOM_SPEED;
+            }
+
+            if (KeyPress.IsKeyDown(Key.Z))
+            {
+                rangeX -= ZOOM_SPEED;
+                rangeY -= ZOOM_SPEED;
+            }
+
+
+            /*
+            int rx1 = TX((float)(x1 * Math.Cos(a) + y1 * Math.Sin(a)), rangeX);
+            int ry1 = TY((float)(x1 * Math.Sin(a) - y1 * Math.Cos(a)), rangeY);
+            int rx2 = TX((float)(x1 * Math.Cos(a) + y2 * Math.Sin(a)), rangeX);
+            int ry2 = TY((float)(x1 * Math.Sin(a) - y2 * Math.Cos(a)), rangeY);
+            int rx3 = TX((float)(x2 * Math.Cos(a) + y2 * Math.Sin(a)), rangeX);
+            int ry3 = TY((float)(x2 * Math.Sin(a) - y2 * Math.Cos(a)), rangeY);
+            int rx4 = TX((float)(x2 * Math.Cos(a) + y1 * Math.Sin(a)), rangeX);
+            int ry4 = TY((float)(x2 * Math.Sin(a) - y1 * Math.Cos(a)), rangeY);
             int c = 0x00ffff;
             
             screen.Line(rx1, ry1, rx2, ry2, c);
             screen.Line(rx2, ry2, rx3, ry3, c);
             screen.Line(rx3, ry3, rx4, ry4, c);
-            screen.Line(rx4, ry4, rx1, ry1, c);
+            screen.Line(rx4, ry4, rx1, ry1, c); */
+
 	    }
 
         int CalculateColor(int r, int g, int b)
@@ -56,29 +107,38 @@ class Game
         }
 
         /// <summary>
-        /// converts values of x from -2 ... 2 to 0 ...640
+        /// converts values of x from -range/2 ... range/2 to 0 ...640
         /// </summary>
         /// <param name="x"></param>
         /// <returns></returns>
-        int TX(float x, float centerX)
+        int TX(float x, float range, float centerX=0)
         {
-            x *= (float)app.Width/screen.width; //where app is the OpenTkApp that defines the screen
-            x += 2 + centerX; //CenterX representing the Cartesian coordinate system with screen center as origin (-2, 2)
-            x *= screen.width >> 2; //Multiply the 4 unit line by 1/4th of the screen width to gain a pixel coordinate
+            x *= Math.Min((float)app.Height / screen.height, (float)app.Width / screen.width); //Mutliply by the mininum of the width or height ratio to ensure a fit
+            x *= (float)app.Height / app.Width; //Multiply by the aspect ratio. (Where app is the OpenTKApp that represents the screen)
+            x += (range/2) + centerX; //CenterX representing the Cartesian coordinate system with screen center as origin (-range, range)
+            x *= screen.width / range; //Mulitply the unit line by the screen width over the range to get a pixel coordinate
             return (int)x;
         }
 
         /// <summary>
-        /// converts values of y from -2 ... 2 to 0 ... 400 (and negates y)
+        /// converts values of y from -range/2 ... range/2 to 0 ... 400 (and negates y)
         /// </summary>
         /// <param name="y"></param>
         /// <returns></returns>
-        int TY(float y, float centerY)
+        int TY(float y, float range, float centerY=0)
         {
-            y = -y;
-            y += 2 - centerY;
-            y *= (screen.height >> 2);
+            y *= Math.Min((float)app.Height / screen.height, (float)app.Width / screen.width); //Mutliply by the mininum of the width or height ratio to ensure a fit
+            y = -y; //Reverse y
+            y += (range/2) - centerY; //Correct again for the Cartesian coordinate system
+            y *= screen.height / range; //Mulitply the unit line by the screen height over the range to get a pixel coordinate
             return (int)y;
+        }
+
+        void HandleKeyBoard()
+        {
+            PrevKeyPress = KeyPress;
+            //Get the current state
+            KeyPress = Keyboard.GetState();
         }
 
 }
