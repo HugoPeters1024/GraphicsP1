6cc235a4817046da5c4c29617209ba966807d754
Hugo Peters <hpeters1024@gmail.com>
2017-05-10 16:05:27 +0200

    Don't fuck with me Git

--- shaders/vs.glsl
+++ shaders/vs.glsl
@@ -4,8 +4,13 @@ in vec3 vNormal;
 in vec3 vColor;
 out vec4 color;
 out vec3 normal;
+out vec3 lightDirection;
+out float falloff;
 
 uniform mat4 M;
+uniform vec3 Ldir;
+uniform vec3 Lpos;
+uniform float intensity;
 
 
 void main()
@@ -13,4 +18,7 @@ void main()
   gl_Position = M * vec4( vPosition, 1.0 );
   color = vec4( vColor, 1.0);
   normal = vec3( (M * vec4( vNormal, 1.0) ) );
+  vec3 lightDistance = Lpos - vPosition;
+  falloff = intensity/dot(lightDistance, lightDistance);
+  lightDirection = normalize(lightDistance);
 }
